91b011c90e09bd12417d1400e7b9890638064df4
asysyifanajla <asysyifanajla246@gmail.com>
2025-12-19 22:56:43 +0700

    deploy django postgres railway

--- backend/talent_profile_project/settings.py
+++ backend/talent_profile_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-86shlxdp&po9#y-j(uj8!zyz8ni92(lq2l71nfi$=r8^e2g#8d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
